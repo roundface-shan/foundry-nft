8d134f70c52097ddaa4ec3f8fa703ba3b82ac4ab
roundface_shan <lice2567614@gmail.com>
2024-10-17 15:46:59 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   .gitignore
    	modified:   broadcast/DeployBasicNft.s.sol/9527/run-latest.json
    	modified:   broadcast/Interactions.s.sol/9527/run-latest.json
    	modified:   foundry.toml
    	new file:   script/DeployMoodNft.s.sol
    	modified:   script/Interactions.s.sol
    	modified:   src/MoodNft.sol

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,6 +6,8 @@ import {Script} from "forge-std/Script.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
+import {MoodNft} from "../src/MoodNft.sol";
+
 contract MintBasicNft is Script {
     string public constant PUG_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
@@ -21,6 +23,36 @@ contract MintBasicNft is Script {
     function mint(address contractAddress) public {
         vm.startBroadcast();
         BasicNft(contractAddress).mintNft(PUG_URI);
+        vm.stopBroadcast();
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployedMoodNft = DevOpsTools
+            .get_most_recent_deployment("MoodNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedMoodNft);
+    }
+
+    function mintNftOnContract(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).mintNFT();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    uint256 public constant TOKEN_ID_TO_FLIP = 1;
+
+    function run() external {
+        address mostRecentlyDeployedMoodNft = DevOpsTools
+            .get_most_recent_deployment("MoodNft", block.chainid);
+        flipMoodNft(mostRecentlyDeployedMoodNft);
+    }
+
+    function flipMoodNft(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).flipMood(TOKEN_ID_TO_FLIP);
         vm.stopBroadcast();
     }
 }
--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -6,6 +6,8 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
+    error MoodNft__CantFlipIfNotOwner();
+
     uint256 private s_tokenCounter;
     string private s_sadSvgImageUri;
     string private s_happySvgImageUri;
@@ -30,6 +32,20 @@ contract MoodNft is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenIdToMood[s_tokenCounter] = Mood.SAD;
         s_tokenCounter++;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        if (
+            getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender
+        ) {
+            revert MoodNft__CantFlipIfNotOwner();
+        }
+
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
